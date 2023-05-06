21042cf025e7e80093b6dcd5e1120272fed9bbad
SooJeong <hansujeong@Soojeongui-MacBookAir.local>
2023-05-06 21:41:38 +0900

    add loading button

--- 2023-1R-WebProgramming/src/App.jsx
+++ 2023-1R-WebProgramming/src/App.jsx
@@ -1,29 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [row, setRow] = useState([]);
-  const res = fetch(
-    "http://openapi.seoul.go.kr:8088/636152524f746e7737316d4a437270/json/RealtimeCityAir/1/25/"
-  ).then(function (res2) {
-    res2.json().then(function (res3) {
-      setRow(res3.RealtimeCityAir.row);
-    });
-  });
+
+  const onClickButton = () => {
+    if (row.length === 0) {
+      fetch(
+        "http://openapi.seoul.go.kr:8088/636152524f746e7737316d4a437270/json/RealtimeCityAir/1/25/"
+      ).then(function (res2) {
+        res2.json().then(function (res3) {
+          setRow(res3.RealtimeCityAir.row);
+        });
+      });
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + Reaalaalaals</h1>
+      <button className="button" onClick={onClickButton}>
+        Loading
+      </button>
       <table>
         <thead>
           <th>이름</th>
@@ -32,9 +29,9 @@ function App() {
           <th>상태</th>
         </thead>
         <tbody>
-          {row.map((obj) => {
+          {row.map((obj, index) => {
             return (
-              <tr>
+              <tr key={index}>
                 <td>{obj.MSRSTE_NM}</td>
                 <td>{obj.PM10}</td>
                 <td>{obj.O3}</td>
