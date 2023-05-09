0e0b10167e4753acedac737808a93d0b9070daae
SooJeong <hansujeong@Soojeongui-MacBookAir.local>
2023-05-09 20:48:01 +0900

    5.9일 과제

--- 2023-1R-WebProgramming/src/App.jsx
+++ 2023-1R-WebProgramming/src/App.jsx
@@ -1,37 +1,77 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [row, setRow] = useState([]);
+  const [count, setCount] = useState(0);
+
+  //click버튼 과제
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+
+    return () => {
+      document.title = "Vite + React";
+    };
+  }, [count]);
 
   const onClickButton = () => {
-    if (row.length === 0) {
-      fetch(
-        "http://openapi.seoul.go.kr:8088/636152524f746e7737316d4a437270/json/RealtimeCityAir/1/25/"
-      ).then(function (res2) {
-        res2.json().then(function (res3) {
-          setRow(res3.RealtimeCityAir.row);
-        });
-      });
-    }
+    setCount((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    console.log("mount or update");
+
+    return () => {
+      console.log("unmount");
+    };
+  });
+
+  //수업시간 실습내용
+  useEffect(() => {
+    console.log("mount only");
+    fetch(
+      "http://openapi.seoul.go.kr:8088/636152524f746e7737316d4a437270/json/RealtimeCityAir/1/25/"
+    ).then(function (res2) {
+      res2.json().then(function (res3) {
+        setRow(res3.RealtimeCityAir.row);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log("update only", row);
+  }, [row]);
+
+  // const onClickButton = () => {
+  //   if (row.length === 0) {
+  //     fetch(
+  //       "http://openapi.seoul.go.kr:8088/636152524f746e7737316d4a437270/json/RealtimeCityAir/1/25/"
+  //     ).then(function (res2) {
+  //       res2.json().then(function (res3) {
+  //         setRow(res3.RealtimeCityAir.row);
+  //       });
+  //     });
+  //   }
+  // };
 
   return (
     <>
-      <button className="button" onClick={onClickButton}>
+      {/* <button className="button" onClick={}>
         Loading
-      </button>
+      </button> */}
       <table>
         <thead>
-          <th>이름</th>
-          <th>PM10</th>
-          <th>O3</th>
-          <th>상태</th>
+          <tr>
+            <th>이름</th>
+            <th>PM10</th>
+            <th>O3</th>
+            <th>상태</th>
+          </tr>
         </thead>
         <tbody>
           {row.map((obj, index) => {
             return (
-              <tr key={index}>
+              <tr key={`row` + index}>
                 <td>{obj.MSRSTE_NM}</td>
                 <td>{obj.PM10}</td>
                 <td>{obj.O3}</td>
@@ -41,6 +81,7 @@ function App() {
           })}
         </tbody>
       </table>
+      <button onClick={onClickButton}> click! count up </button>
     </>
   );
 }
